3b90e5d0ae0bdd21bc6dfc8ddd372462a005f971
Giz <avigiz15@gmail.com>
2020-05-02 21:17:01 +0300

    adding documentations to reader and writer. delete the creation of histogram image in histogram compariion algorithm because the creation of the hist base is happen inside the algorithms calculate score.

--- image_matching_module/writing_module.py
+++ image_matching_module/writing_module.py
@@ -1,142 +1,87 @@
-from typing import List, Set
+from typing import List, Set, Tuple
 import cv2
 import os
 import csv
-from image_matching_module.reading_module import ReadingModule
 from image_matching_module.in_depth_image_pair import InDepthImagePair as InDepthIP
-
 
 
 class WritingModule:
 
     @staticmethod
-    def writing_vintage_to_path(tuple_image_path_and_name, index, vintage_im, filter_name):
-        """writing vintage image to given path, using the vintage index"""
+    def writing_vintage_to_path(tuple_image_path_and_name: Tuple[str, str], index: int, vintage_im, filter_name: str):
+        """
+        This function saves a vintage filter image.
+
+        :param tuple_image_path_and_name: The given tuple of (image path, image name).
+        :param index: The index of the vintage filter.
+        :param vintage_im: The image to save.
+        :param filter_name: The filter name.
+        """
         image_name = tuple_image_path_and_name[1].split(".")[0]
         cv2.imwrite(tuple_image_path_and_name[
                         0] + "\\" + image_name + "\\" + filter_name + "\\" + image_name + filter_name + str(
             index) + ".jpg", vintage_im)
 
     @staticmethod
-    def writing_img_to_path(tuple_image_path_and_name, img, filter_name):
-        """writing image to given path"""
+    def writing_img_to_path(tuple_image_path_and_name: Tuple[str, str], img, filter_name: str):
+        """
+        This function saves a median, cropped and gery scale images.
+
+        :param tuple_image_path_and_name: The given tuple of (image path, image name).
+        :param img: The image to save.
+        :param filter_name: The filter name.
+        """
         image_name = tuple_image_path_and_name[1].split(".")[0]
-        cv2.imwrite(tuple_image_path_and_name[
-                        0] + "\\" + image_name + "\\" + filter_name + "\\" + image_name + filter_name + ".jpg", img)
+        cv2.imwrite(tuple_image_path_and_name[0] + "\\" + image_name + "\\" + filter_name + "\\" + image_name
+                    + filter_name + ".jpg", img)
 
     @staticmethod
-    def writing_img_to_path_mirrored(tuple_image_path_and_name, img, filter_name):
-        """writing image to given path"""
+    def writing_img_to_path_mirrored(tuple_image_path_and_name: Tuple[str, str], img, filter_name: str):
+        """
+        This function saves a mirrored filter image.
+
+        :param tuple_image_path_and_name: The given tuple of (image path, image name).
+        :param img: The image to save.
+        :param filter_name: The filter name.
+        """
         image_name = tuple_image_path_and_name[1].split(".")[0]
-        tt = tuple_image_path_and_name[0] + "\\" + image_name + "\\" + filter_name + "\\" + image_name + filter_name \
-             + ".jpg"
         cv2.imwrite(tuple_image_path_and_name[0] + "\\" + image_name + "\\" + "mirrored" + "\\" + image_name
                     + filter_name + ".jpg", img)
 
     @staticmethod
-    def writing_rotate_to_path(tuple_image_path_and_name, name, folder_name, img):
-        """writing rotated image to path"""
+    def writing_rotate_to_path(tuple_image_path_and_name: Tuple[str, str], angle: int, folder_name: str, img):
+        """
+        This function saves a mirrored filter image.
+
+        :param tuple_image_path_and_name: The given tuple of (image path, image name).
+        :param angle: the angle of the rotated image.
+        :param folder_name: The folder name to save in it the image.
+        :param img: The image to save.
+        """
         image_name = tuple_image_path_and_name[1].split(".")[0]
         img.save(tuple_image_path_and_name[0] + "\\" + image_name + "\\" + folder_name + "\\" + image_name + "rotate"
-                 + str(name) + ".jpg")
+                 + str(angle) + ".jpg")
 
     @staticmethod
-    def create_folders_for_images(tuple_path_and_name_image_list):
-        """creating folder for each picture"""
-        for image_name in tuple_path_and_name_image_list:
+    def create_folders_for_images(list_of_tuple_path_image_name: List[Tuple[str, str]]):
+        """
+        This function create folders by given images names.
+
+        :param list_of_tuple_path_image_name: The given tuple of (path to image , image name)
+        """
+        for image_name in list_of_tuple_path_image_name:
             split = image_name[1].split(".")[0]
             if not os.path.isdir(image_name[0] + "\\" + split):
                 os.mkdir(image_name[0] + "\\" + split)
 
     @staticmethod
-    def there_is_result_folder(path):
-        """writing the histogram csv to given path"""
-        all_folders_in_path = ReadingModule.reading_all_folders_in_given_path(path)
-        for folder_name in all_folders_in_path:
-            if folder_name == "results":
-                return True
-        return False
+    def create_sub_folders_in_pic_folder(images_list: List[Tuple[str, str]]):
+        """
+        This functions creates a folder for each image that been
+         manipulated for testing.
 
-    @staticmethod
-    def writing_histogram_results_to_csv_file(self, path, image_name, list_to_write):
-        if not self.there_is_result_folder(path):
-            self.create_folder_for_results(path, "results")
-        path = path + "\\results"
-
-        method = "method"
-        origin_image = "origin_image"
-        compared_image = "compared_image"
-        score = "score"
-
-        with open(os.path.join(path, image_name.split(".")[0] + 'HistogramOutput.csv'), "w") as f:
-            csvwriter = csv.writer(f, delimiter=' ', lineterminator='\n')
-            csvwriter.writerow(method + "," + origin_image + "," + compared_image + "," + score)
-            for line in list_to_write:
-                csvwriter.writerow(line)
-        # print("finish with histogram working on image number: " + image_name)
-
-    @staticmethod
-    def writing_features_results_to_csv_file(self, path, image_name, list_to_write):
-        """writing the features csv to given path"""
-        if not self.there_is_result_folder(path):
-            self.create_folder_for_results(path, "results")
-        path = path + "\\results"
-
-        origin_image_name = "origin_image_name"
-        number_of_features = "number_of_features"
-        compared_image = "compared_image"
-        number_of_matchers = "number_of_matchers"
-        score = "score"
-
-        with open(os.path.join(path, image_name.split(".")[0] + 'FaeturesOutput.csv'), "w") as f:
-            csvwriter = csv.writer(f, delimiter=' ', lineterminator='\n')
-            csvwriter.writerow(origin_image_name + "," + number_of_features + "," + compared_image + "," +
-                               number_of_features + "," + number_of_matchers + "," + score)
-            for line in list_to_write:
-                csvwriter.writerow(line)
-        # print("finish with features working on image number: " + image_name)
-
-    @staticmethod
-    def create_folder_for_results(path, folder_name):
-        """creating folder for results"""
-        os.mkdir(path + "\\" + folder_name)
-
-    @staticmethod
-    def write_histogram_similar_images(path, list_to_write):
-        path = path + "\\results"
-
-        origin_image_path = "origin_image_path"
-        origin_image_name = "origin_image_name"
-        to_compare_image_path = "to_compare_image_path"
-        to_compare_image_name = "to_compare_image_name"
-        score = "score"
-
-        with open(os.path.join(path, 'HistogramResults.csv'), "w") as f:
-            csvwriter = csv.writer(f, delimiter=' ', lineterminator='\n')
-            csvwriter.writerow(origin_image_path + "," + origin_image_name + "," + to_compare_image_path +
-                               "," + to_compare_image_name + "," + score)
-            for line in list_to_write:
-                csvwriter.writerow(line)
-
-    @staticmethod
-    def write_features_similar_images_results(path, list_to_write):
-        path = path + "\\results"
-
-        origin_image_path = "origin_image_path"
-        origin_image_name = "origin_image_name"
-        to_compare_image_path = "to_compare_image_path"
-        to_compare_image_name = "to_compare_image_name"
-        score = "score"
-
-        with open(os.path.join(path, 'FeaturesResults.csv'), "w") as f:
-            csvwriter = csv.writer(f, delimiter=' ', lineterminator='\n')
-            csvwriter.writerow(origin_image_path + "," + origin_image_name + "," + to_compare_image_path + "," +
-                               to_compare_image_name + "," + score)
-            for line in list_to_write:
-                csvwriter.writerow(line)
-
-    @staticmethod
-    def create_sub_folders_in_pic_folder(images_list):
+        :param images_list: The given tuple of (path to image , image name)
+        """
         for image in images_list:
             path_to_pic_folder = image[0] + "\\" + image[1].split(".")[0]
             if not os.path.isdir(path_to_pic_folder + "\\" + "cropped"):
@@ -147,48 +92,71 @@ class WritingModule:
                 os.mkdir(path_to_pic_folder + "\\" + "vintage")
                 os.mkdir(path_to_pic_folder + "\\" + "greyscale")
 
-    def write_store_results_to_file(self, stores_path, in_depth_results: List[InDepthIP]):
-        file_name = stores_path + "/results.csv"
+    @staticmethod
+    def write_store_results_to_file(store_path: str, in_depth_results: List[InDepthIP]):
+        """
+        This function write the results of a store to csv file.
+
+        :param store_path: The path to the store.
+        :param in_depth_results: The results who needed to be written.
+        """
+        file_name = store_path + "/results.csv"
         if os.path.isfile(file_name):
             os.remove(file_name)
-        with open(file_name,'w', newline='') as csv_file:
-            csvwriter = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+        with open(file_name, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
             origin_image_path = "origin_image_path"
             origin_image_name = "origin_image_name"
             to_compare_image_path = "to_compare_image_path"
             to_compare_image_name = "to_compare_image_name"
             score = "score"
-            csvwriter.writerow([origin_image_path, origin_image_name, to_compare_image_path,
+            csv_writer.writerow([origin_image_path, origin_image_name, to_compare_image_path,
                                 to_compare_image_name, score])
             for row in in_depth_results:
-                csvwriter.writerow([row.customer_image_path, row.customer_image_name, row.store_image_path,
+                csv_writer.writerow([row.customer_image_path, row.customer_image_name, row.store_image_path,
                                     row.store_image_name, str(row.final_score)])
         csv_file.close()
 
-    def write_final_results_file(self,stores_path, in_depth_results : List[InDepthIP]):
+    @staticmethod
+    def write_final_results_file(stores_path: str, in_depth_results: List[InDepthIP]):
+        """
+        This function write the final results file.
+
+        :param stores_path: The path to the folder of the stores.
+        :param in_depth_results: The results to be written.
+        """
         file_name = stores_path + "/final_results.csv"
         with open(file_name, 'w', newline='') as csv_file:
-            csvwriter = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+            csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
             origin_image_path = "origin_image_path"
             origin_image_name = "origin_image_name"
             to_compare_image_path = "to_compare_image_path"
             to_compare_image_name = "to_compare_image_name"
             score = "score"
-            csvwriter.writerow([origin_image_path,origin_image_name , to_compare_image_path
-                              , to_compare_image_name , score])
+            csv_writer.writerow([origin_image_path, origin_image_name, to_compare_image_path,
+                                 to_compare_image_name, score])
             for row in in_depth_results:
-                csvwriter.writerow([row.customer_image_path , row.customer_image_name, row.store_image_path
-                                   , row.store_image_name , str(row.final_score)])
+                csv_writer.writerow([row.customer_image_path, row.customer_image_name, row.store_image_path,
+                                     row.store_image_name, str(row.final_score)])
         csv_file.close()
 
-    def merge_final_results_file_and_new_results(self, stores_path, in_depth_results: List[InDepthIP],
+    @staticmethod
+    def merge_final_results_file_and_new_results(stores_path: str, in_depth_results: List[InDepthIP],
                                                  prev_store_results: Set = None):
+        """
+        This function merge the current final results file with the new results from a store
+        and write the results to a new file of final results.
+
+        :param stores_path: The folder of all stores path.
+        :param in_depth_results: The list of the results of the store.
+        :param prev_store_results: The results of the previous results of the store.
+        """
         prev_store_results_exists = prev_store_results is not None
         file_name = stores_path + "/final_results.csv"
         file_name_tmp = stores_path + "/final_results_tmp.csv"
         in_depth_results_index = 0
         first = True
-        self.write_coulmn_names(stores_path)
+        WritingModule.write_column_names(stores_path)
         with open(file_name, newline='') as csvfile:
             line = csvfile.readline()
             while line and in_depth_results_index < len(in_depth_results):
@@ -205,54 +173,86 @@ class WritingModule:
                 spllited_line = line.split(",")
                 old_score = spllited_line[4]
                 if float(old_score) > in_depth_results[in_depth_results_index]:
-                    self.write_spllitted_line_to_csv(spllited_line, file_name_tmp)
+                    WritingModule.write_spllitted_line_to_csv(spllited_line, file_name_tmp)
                     line = csvfile.readline()
                 else:
-                    self.write_object_to_csv(in_depth_results[in_depth_results_index], file_name_tmp)
+                    WritingModule.write_object_to_csv(in_depth_results[in_depth_results_index], file_name_tmp)
                     in_depth_results_index += 1
             while line and line != '"\n':
                 line = line.rstrip()
                 spllited_line = line.split(",")
-                self.write_spllitted_line_to_csv(spllited_line, file_name_tmp)
+                WritingModule.write_spllitted_line_to_csv(spllited_line, file_name_tmp)
                 line = csvfile.readline()
             while in_depth_results_index < len(in_depth_results):
-                self.write_object_to_csv(in_depth_results[in_depth_results_index], file_name_tmp)
+                WritingModule.write_object_to_csv(in_depth_results[in_depth_results_index], file_name_tmp)
                 in_depth_results_index += 1
         csvfile.close()
         os.remove(file_name)
-        os.rename(file_name_tmp,file_name)
+        os.rename(file_name_tmp, file_name)
 
-    def update_final_results_file(self, stores_path, in_depth_results: List[InDepthIP], prev_store_results=None):
+    @staticmethod
+    def update_final_results_file(stores_path: str, in_depth_results: List[InDepthIP], prev_store_results=None):
+        """
+        This function write the final results file, if it is already exists merge the new results with the
+        final results, else write the file as new one.
+
+        :param stores_path: The path to the stores folder.
+        :param in_depth_results: The results of a store
+        :param prev_store_results: The results of the previous store, if there is no
+        previous results will be assigned as None
+        """
         file_name = stores_path + "/final_results.csv"
         if os.path.isfile(file_name):
             if prev_store_results is None:
-                self.merge_final_results_file_and_new_results(stores_path, in_depth_results)
+                WritingModule.merge_final_results_file_and_new_results(stores_path, in_depth_results)
             else:
-                self.merge_final_results_file_and_new_results(stores_path,in_depth_results, prev_store_results)
+                WritingModule.merge_final_results_file_and_new_results(stores_path, in_depth_results,
+                                                                       prev_store_results)
         else:
-            self.write_final_results_file(stores_path, in_depth_results)
+            WritingModule.write_final_results_file(stores_path, in_depth_results)
 
-    def write_spllitted_line_to_csv(self, spllited_line, file_name_tmp):
+    @staticmethod
+    def write_spllitted_line_to_csv(splitted_line: List[str], file_name_tmp):
+        """
+        This function write the splitted line to the file.
+
+        :param splitted_line: The given line to write.
+        :param file_name_tmp: The given file name to write to.
+        """
         with open(file_name_tmp, 'a', newline='') as csv_file:
-            csvwriter = csv.writer(csv_file, delimiter=',', lineterminator='\n')
-            csvwriter.writerow([spllited_line[0], spllited_line[1], spllited_line[2], spllited_line[3], spllited_line[4]])
+            csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+            csv_writer.writerow([splitted_line[0], splitted_line[1], splitted_line[2],
+                                 splitted_line[3], splitted_line[4]])
         csv_file.close()
 
-    def write_object_to_csv(self, result: InDepthIP, file_name_tmp):
+    @staticmethod
+    def write_object_to_csv(result: InDepthIP, file_name_tmp):
+        """
+        This function write an object to a file.
+        :param result: The given object to write.
+        :param file_name_tmp: The given file name to wrtie to.
+        """
         with open(file_name_tmp, 'a', newline='') as csv_file:
-            csvwriter = csv.writer(csv_file, delimiter=',', lineterminator='\n')
-            csvwriter.writerow([result.customer_image_path, result.customer_image_name,result.store_image_path,
+            csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+            csv_writer.writerow([result.customer_image_path, result.customer_image_name, result.store_image_path,
                                 result.store_image_name, result.final_score])
         csv_file.close()
 
-    def write_coulmn_names(self, stores_path):
+    @staticmethod
+    def write_column_names(stores_path: str):
+        """
+        This function write The column names of the temporary results file.
+
+        :param stores_path: The path to the folder of the stores.
+        """
         file_name = stores_path + "/final_results_tmp.csv"
         with open(file_name, 'w', newline='') as csv_file:
-            csvwriter = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+            csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
             origin_image_path = "origin_image_path"
             origin_image_name = "origin_image_name"
             to_compare_image_path = "to_compare_image_path"
             to_compare_image_name = "to_compare_image_name"
             score = "score"
-            csvwriter.writerow([origin_image_path, origin_image_name, to_compare_image_path,
+            csv_writer.writerow([origin_image_path, origin_image_name, to_compare_image_path,
                                 to_compare_image_name, score])
+        csv_file.close()
