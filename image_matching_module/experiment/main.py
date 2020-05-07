809ccf4d73cee5d6b974d43fa97c5bf98f5103b4
Giz <avigiz15@gmail.com>
2020-05-07 20:29:11 +0300

    updated main

--- image_matching_module/experiment/main.py
+++ image_matching_module/experiment/main.py
@@ -6,6 +6,7 @@ from image_matching_module.algorithms.chisquared_comparison_algorithm import Chi
 from image_matching_module.algorithms.orb_feature_comparisson_algorithm import ORBFeatureComparisonAlgorithm
 from image_matching_module.experiment.experiments.comparison_experiment import ComparisonExperiment
 from image_matching_module.algorithms.intersection_comparison_algorithm import IntersectionComparisonAlgorithm
+from image_matching_module.experiment.experiments.fn_detection_exeperiment import FNDetectionExperiment
 from image_matching_module.experiment.experiments.precision_recall_experiment import PrecisionRecallExperiment
 import itertools
 from itertools import chain, permutations
@@ -20,9 +21,11 @@ def main():
     correlation = CorrelationComparisonAlgorithm()
     intersection = IntersectionComparisonAlgorithm()
     bhattacharyya = BhattacharyyaComparisonAlgorithm()
-    orb_features = ORBFeatureComparisonAlgorithm(0.6)
+    orb_features = ORBFeatureComparisonAlgorithm()
     random = RandomComparisonAlgorithm()
     precision_recall_experiment = PrecisionRecallExperiment()
+    fn_experiment = FNDetectionExperiment()
+
     algorithms = [chi_squre,correlation,intersection,bhattacharyya,random]
     # thresholds = [0.65,0.66,0.67,0.68,0.69,0.7,0.71,0.72,0.73,0.74,0.75,0.76,0.77,0.78,0.79,0.8,0.81,0.82,0.83,0.84,0.85,0.86,0.87,0.88,0.89,0.9]
     thresholds = []
@@ -37,15 +40,32 @@ def main():
                Decimal('0.34'), Decimal('0.4'), Decimal('0.4'), Decimal('0.5'),
                Decimal('0.6'), Decimal('0.7'), Decimal('0.75'), Decimal('0.8'),
                Decimal('0.9')]
-    new_results = get_all_permutations(numbers)
+    #new_results = get_all_permutations(numbers)
 
-    algorithms_weights_list = []
-    for new_result in new_results:
-        algorithms_weights_list.append([(bhattacharyya, new_result[0]), (intersection, new_result[1]),
-                          (correlation, new_result[2])])
+    initials_algorithms_weights_threshold = []
+    in_depth_algorithms_weights_threshod = []
 
-    csv_path = "F:/avi/image_matching_module/results/precision_recall_results/"
-    precision_recall_experiment.run_multiple_advanced_experiments(csv_path,algorithms_weights_list,thresholds)
+    #for new_result in new_results:
+    #    initials_algorithms_weights_list.append([(bhattacharyya, new_result[0]), (intersection, new_result[1]),
+    #                      (correlation, new_result[2])])
+
+    combained_initial_threshold = Decimal('0.87')
+    combained_in_depth_threshold = Decimal('0.64')
+
+    combined_initial_score_weight = Decimal('0.5')
+
+
+    initials_algorithms_weights_threshold.append((bhattacharyya , Decimal('0.7'),  Decimal('0.85')))
+    initials_algorithms_weights_threshold.append((intersection , Decimal('0.05'),  Decimal('0.94')))
+    initials_algorithms_weights_threshold.append((correlation , Decimal('0.87'),  Decimal('0.98')))
+
+    in_depth_algorithms_weights_threshod.append((orb_features, Decimal('1.0'), Decimal('0.57')))
+
+    csv_path = "F:/avi/finalProjectResults/AllResultsMac/"
+
+
+    fn_experiment.run_fn_detection_experiment(csv_path,initials_algorithms_weights_threshold,in_depth_algorithms_weights_threshod
+                                             ,combained_initial_threshold,combained_in_depth_threshold,combined_initial_score_weight)
 
 
 def get_all_permutations(numbers):
@@ -62,11 +82,11 @@ def get_all_permutations(numbers):
 
 
 def test_image_matching():
-    image_maching = ImageMatching("F:/avi/test_image_maching/customer","F:/avi/test_image_maching/stores",ImageMatchingConfiguration(),
-                                  ReadingModule(),WritingModule())
+    image_maching = ImageMatching("F:/avi/test_image_maching/customer","F:/avi/test_image_maching/stores",ImageMatchingConfiguration(),)
     image_maching.run_matching_for_all_stores()
 
 
+
 if __name__ == '__main__':
-    #main()
-    test_image_matching()
+    main()
+    #test_image_matching()
