6677d6c4bf6fb8d3edd987509c98e6fe94f15034
Giz <avigiz15@gmail.com>
2020-05-11 17:54:04 +0300

    fix manipulation imports

--- image_matching_module/oldstuff/manipulation_module.py
+++ image_matching_module/oldstuff/manipulation_module.py
@@ -3,13 +3,12 @@ import cv2
 import numpy as np
 from PIL import Image
 
-import image_matching_module.reading_files_module
-import image_matching_module.writing_utils
 
-
+from image_matching_module.reading_utils import ReadingUtils
+from image_matching_module.writing_utils import WritingUtils
 #vintage filter
 def vintage_filter(tuple_image_path_and_name, index):
-    im = reading_files_module.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
+    im = ReadingUtils.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
     rows, cols = im.shape[:2]
     # Create a Gaussian filter
     kernel_x = cv2.getGaussianKernel(cols, cols - 100)
@@ -20,24 +19,24 @@ def vintage_filter(tuple_image_path_and_name, index):
     # for each channel in the input image, we will apply the above filter
     for i in range(index):
         vintage_im[:, :, i] = vintage_im[:, :, i] * filter
-    writing_module.writing_vintage_to_path(tuple_image_path_and_name, index, vintage_im, "vintage")
+    WritingUtils.writing_vintage_to_path(tuple_image_path_and_name, index, vintage_im, "vintage")
 
 #median filter
 def median_filter (tuple_image_path_and_name):
-    img = reading_files_module.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
+    img = ReadingUtils.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
     median = cv2.medianBlur(img,7)
-    writing_module.writing_img_to_path(tuple_image_path_and_name, median, "median")
+    WritingUtils.writing_img_to_path(tuple_image_path_and_name, median, "median")
 
 #roteting image.
 def rotate(tuple_image_path_and_name,delta_given):
     rotate = 0
     delta = delta_given
-    image = reading_files_module.reading_image_from_tuple_path_using_image(tuple_image_path_and_name)
+    image = ReadingUtils.reading_image_from_tuple_path_using_image(tuple_image_path_and_name)
     for i in range(0,int(360 / delta)):
         rotated_image = image.rotate(rotate, fillcolor='white', expand=True)
         name = rotate
         rotate += delta
-        writing_module.writing_rotate_to_path(tuple_image_path_and_name, name, "rotated", rotated_image)
+        WritingUtils.writing_rotate_to_path(tuple_image_path_and_name, name, "rotated", rotated_image)
 
 
 def rgb2gray(rgb):
@@ -46,21 +45,21 @@ def rgb2gray(rgb):
 
 #grey scale
 def greyscale(tuple_image_path_and_name):
-    image = reading_files_module.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
+    image = ReadingUtils.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
     gray = rgb2gray(image)
-    writing_module.writing_img_to_path(tuple_image_path_and_name, gray, "greyscale")
+    WritingUtils.writing_img_to_path(tuple_image_path_and_name, gray, "greyscale")
 
 
 #crop  need to fix the sizes of the crop, first arg is y axis and then x axis
 def crop(tuple_image_path_and_name):
-    image = reading_files_module.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
+    image = ReadingUtils.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
     (h1, w1) = image.shape[:2]
     top_left_height = int(h1 / 8)
     button_right_height = int (3 * h1 / 4) + top_left_height
     top_left_weight =  int(w1 / 8)
     button_right_weight = int (3 * w1 / 4) + top_left_weight
     crop_img = image[top_left_height : button_right_height,top_left_weight : button_right_weight]
-    writing_module.writing_img_to_path(tuple_image_path_and_name, crop_img, "cropped")
+    WritingUtils.writing_img_to_path(tuple_image_path_and_name, crop_img, "cropped")
 
 
 
@@ -82,13 +81,13 @@ def flipped_image_horizontal_and_vertical(image):
 
 #creating filipped (mirrored) image.
 def flipped_image(tuple_image_path_and_name):
-    image = reading_files_module.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
-    flipVertical = flipped_image_vertical(image)
-    writing_module.writing_img_to_path_mirrored(tuple_image_path_and_name, flipVertical, "mirroredVertical")
-    flipHorizontal = flipped_image_horizontal(image)
-    writing_module.writing_img_to_path_mirrored(tuple_image_path_and_name, flipHorizontal, "mirroredHorizontal")
-    flipVerAndHori = flipped_image_horizontal_and_vertical(image)
-    writing_module.writing_img_to_path_mirrored(tuple_image_path_and_name, flipVerAndHori, "mirroredVerAndHori")
+    image = ReadingUtils.reading_image_from_tuple_path_open_cv(tuple_image_path_and_name)
+    flip_vertical = flipped_image_vertical(image)
+    WritingUtils.writing_img_to_path_mirrored(tuple_image_path_and_name, flip_vertical, "mirroredVertical")
+    flip_horizontal = flipped_image_horizontal(image)
+    WritingUtils.writing_img_to_path_mirrored(tuple_image_path_and_name, flip_horizontal, "mirroredHorizontal")
+    flip_ver_and_hori = flipped_image_horizontal_and_vertical(image)
+    WritingUtils.writing_img_to_path_mirrored(tuple_image_path_and_name, flip_ver_and_hori, "mirroredVerAndHori")
 
 
 #creating filters for an image.
@@ -115,9 +114,9 @@ def change_images_name(source,images_list):
 
 def main_func_manipulation(source,images_list):
     #change_images_name(source,images_list)
-    images_list = reading_files_module.get_images_names_in_folder(source)
-    writing_module.create_folders_for_images(images_list)
-    writing_module.create_sub_folders_in_pic_folder(images_list)
+    images_list = ReadingUtils.get_images_names_in_folder(source)
+    WritingUtils.create_folders_for_images(images_list)
+    WritingUtils.create_sub_folders_in_pic_folder(images_list)
     print("folders created.")
     print("----------------------------")
     create_filter_for_images(source,images_list)
